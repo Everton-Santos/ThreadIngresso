d0537303671ddd8bfa0ca559493a787ca69c8e19
Everton Santos <everton_santos_01@hotmail.com>
2021-03-16 21:24:48 -0300

    Primeiro Commit

---
+++ src/application/SistemaCompraIngresso.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadIngresso;
+
+public class SistemaCompraIngresso {
+
+	public static void main(String[] args) {
+		
+		Semaphore espera = new Semaphore(1);
+			
+		for(int idComprador = 1; idComprador <= 300; idComprador++) {
+			Thread ingressos = new ThreadIngresso(idComprador, espera);
+			ingressos.start();
+			
+		}
+
+	}
+
+}
